0309fa8aee92e1a2795d6e566f5b8df4fbac4768
Anna Toczek <ania4134@gmail.com>
2022-03-14 21:17:40 +0000

    2nd commit

--- src/main/java/com/kodilla/checkers/App.java
+++ src/main/java/com/kodilla/checkers/App.java
@@ -4,8 +4,9 @@ public class App {
     public static void main(String[] args) {
         Board board = new Board()                                                                                   ;
         board.initBoard();
+        Pawn bPawn1 = new Pawn(FigureColor.BLACK);
         System.out.println(board);
-        boolean result = board.move(2,2,3,3);
+        boolean result = board.move(0,6,1,5);
         System.out.println(board);
         System.out.println(result);
     }
